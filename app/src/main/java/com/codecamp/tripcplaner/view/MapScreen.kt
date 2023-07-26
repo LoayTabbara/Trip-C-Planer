2eb10bbfd7e7df56ac1715115a2b073c166a575d
LoayTabbara <loaytabbara@gmail.com>
2023-07-26 11:43:05 +0200

    added floating buttons

--- app/src/main/java/com/codecamp/tripcplaner/view/MapScreen.kt
+++ app/src/main/java/com/codecamp/tripcplaner/view/MapScreen.kt
@@ -4,26 +4,52 @@ import android.Manifest
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.app.DatePickerDialog
+import android.graphics.drawable.Icon
 import android.location.Location
 import android.util.Log
 import android.widget.DatePicker
+import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Create
+import androidx.compose.material.icons.filled.Done
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.BitmapPainter
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.codecamp.tripcplaner.MAPS_API_KEY
 import com.codecamp.tripcplaner.MainActivity
+import com.codecamp.tripcplaner.R
 import com.codecamp.tripcplaner.view.widgets.PermissionSnackbar
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.rememberMultiplePermissionsState
@@ -40,7 +66,7 @@ import com.google.maps.android.compose.MapUiSettings
 import com.google.maps.android.compose.rememberCameraPositionState
 import java.util.Calendar
 
-@OptIn(ExperimentalPermissionsApi::class)
+@OptIn(ExperimentalPermissionsApi::class, ExperimentalMaterial3Api::class)
 @SuppressLint("MissingPermission")
 @Composable
 fun MapScreen(
@@ -54,74 +80,119 @@ fun MapScreen(
             Manifest.permission.INTERNET
         )
     )
-    Column() {
-        PermissionSnackbar(permissionsState = permissionsState)
+    val context = LocalContext.current
+    Scaffold(
+        floatingActionButtonPosition = FabPosition.Center,
+        floatingActionButton = {
+            FloatingActionButton(onClick = { Toast.makeText(context, "Created!", Toast.LENGTH_SHORT).show() }) {
+                Text(text = "Create Trip", )
 
-        val uiSettings = remember {
-            MapUiSettings(
-                myLocationButtonEnabled = permissionsState.allPermissionsGranted,
-                mapToolbarEnabled = true,
-                compassEnabled = true,
-                scrollGesturesEnabled = true
-            )
+            }
+            FloatingActionButton(modifier = Modifier.offset(100.dp),containerColor= Color(0xFF006400), onClick = { Toast.makeText(context, "Added!", Toast.LENGTH_SHORT).show() }) {
+                Icon(Icons.Filled.Add, "Add")
+
+            }
         }
-        val properties by remember {
-            mutableStateOf(
-                MapProperties(
-                    isMyLocationEnabled = permissionsState.allPermissionsGranted,
-                    isBuildingEnabled = true
+    ) {
+
+        Column(
+            Modifier
+                .fillMaxWidth()
+                .padding(it)
+        ) {
+            PermissionSnackbar(permissionsState = permissionsState)
+
+            val uiSettings = remember {
+                MapUiSettings(
+                    myLocationButtonEnabled = permissionsState.allPermissionsGranted,
+                    mapToolbarEnabled = true,
+                    compassEnabled = true,
+                    scrollGesturesEnabled = true
                 )
-            )
-        }
-        val context = LocalContext.current
+            }
+            val properties by remember {
+                mutableStateOf(
+                    MapProperties(
+                        isMyLocationEnabled = permissionsState.allPermissionsGranted,
+                        isBuildingEnabled = true
+                    )
+                )
+            }
+            val context = LocalContext.current
 
-        val fusedLocationProviderClient =
-            remember { LocationServices.getFusedLocationProviderClient(context) }
+            val fusedLocationProviderClient =
+                remember { LocationServices.getFusedLocationProviderClient(context) }
 
-        var lastKnownLocation by remember {
-            mutableStateOf<Location?>(null)
-        }
+            var lastKnownLocation by remember {
+                mutableStateOf<Location?>(null)
+            }
 
-        var deviceLatLng by remember {
-            mutableStateOf(LatLng(52.52437, 13.41053))
-        }
+            var deviceLatLng by remember {
+                mutableStateOf(LatLng(52.52, 13.41))
+            }
 
-        val cameraPositionState = rememberCameraPositionState {
-            position = CameraPosition.fromLatLngZoom(deviceLatLng, 13f)
-        }
+            val cameraPositionState = rememberCameraPositionState {
+                position = CameraPosition.fromLatLngZoom(deviceLatLng, 13f)
+            }
 
-        if (permissionsState.allPermissionsGranted) {
-            val locationResult = fusedLocationProviderClient.lastLocation
-            locationResult.addOnCompleteListener(context as MainActivity) { task ->
-                if (task.isSuccessful) {
-                    // Set the map's camera position to the current location of the device.
-                    lastKnownLocation = task.result
-                    deviceLatLng =
-                        LatLng(lastKnownLocation!!.latitude, lastKnownLocation!!.longitude)
-                    cameraPositionState.position = CameraPosition.fromLatLngZoom(deviceLatLng, 18f)
-                } else {
-                    Log.d("TAG", "Current location is null. Using defaults.")
-                    Log.e("TAG", "Exception: %s", task.exception)
+            if (permissionsState.allPermissionsGranted) {
+                val locationResult = fusedLocationProviderClient.lastLocation
+                locationResult.addOnCompleteListener(context as MainActivity) { task ->
+                    if (task.isSuccessful) {
+                        // Set the map's camera position to the current location of the device.
+                        lastKnownLocation = task.result
+                        deviceLatLng =
+                            LatLng(lastKnownLocation!!.latitude, lastKnownLocation!!.longitude)
+                        cameraPositionState.position =
+                            CameraPosition.fromLatLngZoom(deviceLatLng, 18f)
+                    } else {
+                        Log.d("TAG", "Current location is null. Using defaults.")
+                        Log.e("TAG", "Exception: %s", task.exception)
+                    }
                 }
             }
-        }
+//        mutableStateListOf<Composable>(StopRow(),StopRow())
+            Column(
+                Modifier
+                    .background(Color.White)
+                    .fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Row(
+                    Modifier
+                        .fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    CityPicker()
 
-        Column() {
-            Row() {
-                CityPicker()
-                DatePicker()
+                    DatePicker()
+
+                }
+//            var uiState = StateFlow
+//            LazyColumn {
+//                items(items = uiState.listItems, key = { item -> item.getKey() }) { listItem ->
+//                    if (listItem == uiState.listItems.first()) {
+//                        AddMoreHeader(onClick = {headerClick()})
+//                    }
+//                    ListItem(listItem)
+//                    if (listItem == uiState.listItems.last()) {
+//                        AddMoreFooter(onClick = {viewModel.footerClick()})
+//                    }
+//                }
+//            }
+
             }
-        }
-        GoogleMap(
-            modifier = Modifier.fillMaxSize(),
-            cameraPositionState = cameraPositionState,
-            uiSettings = uiSettings,
-            properties = properties,
-            onMapClick = {
-                Log.d("TestTTTT", "onMapClick: $it")
+            GoogleMap(
+                modifier = Modifier.fillMaxSize(),
+                cameraPositionState = cameraPositionState,
+                uiSettings = uiSettings,
+                properties = properties,
+                onMapClick = {
+                    Log.d("TestTTTT", "onMapClick: $it")
 
-            },
-        ) {
+                },
+            ) {
+            }
         }
 
 
@@ -129,8 +200,28 @@ fun MapScreen(
 }
 
 @Composable
+fun StopRow() {
+    CityPicker()
+    DatePicker()
+    RemoveButton()
+}
+
+@Composable
+fun RemoveButton() {
+    Button(
+        onClick = { /*TODO*/ }, colors = ButtonDefaults.buttonColors(
+            containerColor = Color.Transparent, contentColor = Color.Red
+        )
+    ) {
+        Text(text = "\uD83D\uDDD1", fontSize = 24.sp, textAlign = TextAlign.Center)
+    }
+}
+
+@Composable
 fun CityPicker() {
+    val id = remember { mutableStateOf("") }
     val context = LocalContext.current
+    var selectedPlace by remember { mutableStateOf("") }
 
     val intentLauncher = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.StartActivityForResult()
@@ -139,33 +230,29 @@ fun CityPicker() {
             Activity.RESULT_OK -> {
                 it.data?.let {
                     val place = Autocomplete.getPlaceFromIntent(it)
-                    Log.i("MAP_ACTIVITY", "Place: ${place.name}, ${place.id}")
+                    id.value = place.id as String
+                    Log.i("MAP_ACTIVITY", "Place: ${place.name}, ${place.latLng}")
+
+                    selectedPlace = place.name as String
                 }
-            }
-            2 -> {
-                it.data?.let {
-                    val status = Autocomplete.getStatusFromIntent(it)
-                    Log.i("MAP_SCREEN", "Status: ${status.statusMessage}")
-                }
-            }
-            Activity.RESULT_CANCELED -> {
-                // The user canceled the operation.
             }
         }
     }
 
     val launchMapInputOverlay = {
         Places.initialize(context, MAPS_API_KEY)
-        val fields = listOf(Place.Field.ID, Place.Field.NAME)
+        val fields = listOf(Place.Field.ID, Place.Field.NAME, Place.Field.LAT_LNG)
         val intent = Autocomplete
             .IntentBuilder(AutocompleteActivityMode.OVERLAY, fields)
             .build(context)
         intentLauncher.launch(intent)
     }
-
-    Column {
+    Column(Modifier.background(Color.Transparent)) {
         Button(onClick = launchMapInputOverlay) {
-            Text("Select Location")
+//            Text("Select Location")
+            Text(text = selectedPlace.ifEmpty {
+                "Select Location"
+            })
         }
     }
 }
@@ -190,9 +277,7 @@ fun DatePicker() {
         }
     ) {
         Text(
-            text = if (selectedDateText.isNotEmpty()) {
-                selectedDateText
-            } else {
+            text = selectedDateText.ifEmpty {
                 "Date"
             }
         )
